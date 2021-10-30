f1ce3081549a1f6d39efa58e772b52def2103d6d
Svetozar99 <svetozarb999@gmail.com>
2021-10-30 20:43:42 +0200

    initial commit

---
+++ src/main/java/com/example/PraksaDrustvenaMreza/PraksaDrustvenaMrezaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PraksaDrustvenaMreza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraksaDrustvenaMrezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraksaDrustvenaMrezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PraksaDrustvenaMreza/PraksaDrustvenaMrezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PraksaDrustvenaMreza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraksaDrustvenaMrezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
